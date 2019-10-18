be9f2c015d1bce33f68f1900f2b6a022110603ba
Woshiajuana <zhigang.chen@owulia.com>
2019-10-18 10:33:55 +0800

    subcode version 0.0.1

---
+++ config/config.default.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * egg-wow-email default config
+ * @author Ajuan <979703986@qq.com>
+ * @member Config#email
+ * @property {String} SOME_KEY - some description
+ */
+exports.email = {
+    app: true,
+    agent: false,
+};
